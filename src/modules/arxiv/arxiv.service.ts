ce8abe138428c0ae28f5aa4fe2b364a689307e15
Aleksandr Fedorov <fedorov@farzoom.com>
2021-05-30 03:11:02 +0300

    parse xml from arxiv

--- src/modules/arxiv/arxiv.service.ts
+++ src/modules/arxiv/arxiv.service.ts
@@ -1,9 +1,8 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 import { Injectable } from '@nestjs/common';
+import { DOMParser } from 'xmldom';
 import { Cron, CronExpression } from '@nestjs/schedule';
-import cheerio from 'cheerio';
 import got from 'got/dist/source';
-import { sampleSize } from 'lodash';
 const { Translate } = require('@google-cloud/translate').v2;
 import { MessageService } from '../message/message.service';
 
@@ -11,21 +10,27 @@ import { MessageService } from '../message/message.service';
 export class ArxivService {
   constructor(private readonly messageService: MessageService) {}
 
-  @Cron(CronExpression.EVERY_DAY_AT_8PM)
+  //@Cron(CronExpression.EVERY_DAY_AT_8PM)
+  @Cron('5 * * * * *')
   async publishArticles() {
     try {
       // Get articles
       const articles = await this.fetchArticles();
+      console.log(articles);
       const articlesTitles: string[] = [];
-      for (const article of articles) {
-        const title = this.getTitle(article.title);
-        articlesTitles.push(title);
+      const articlesIds: string[] = [];
+      for (let i = 0; i < articles.feed.entry.length; i++) {
+         let id = articles.feed.entry[i].id;
+         let title = articles.feed.entry[i].title;
+         let summary = articles.feed.entry[i].summary.substring(0, 150) + '...';
+         title = `<b>${title}</b>\n📚 ${summary}`;
+         articlesIds.push(id);
+         articlesTitles.push(title);
       }
       // Publish them
-      let publishText =
-        'Новые #Arxiv статьи на сегодня:\n\n';
+      let publishText = 'Новые #Arxiv статьи на сегодня:\n\n';
       for (let i = 0; i < 3; i++) {
-        publishText += `${articlesTitles[i]}\n🌐 ${articles[i].id}\n\n`;
+        publishText += `${articlesTitles[i]}\n🌐 ${articlesIds[i]}\n\n`;
       }
       publishText += '<i>подготовлено @ArxivArticlesDailyBot</i>';
       await this.messageService.sendMessage(publishText);
@@ -34,13 +39,17 @@ export class ArxivService {
     }
   }
 
-  private fetchArticles() {
-    const response = got(
-      'http://export.arxiv.org/api/query?search_query=all:Physics',
+  private async fetchArticles() {
+    const response = await got(
+      'http://export.arxiv.org/api/query?search_query=all:Artificial+Intelligence&sortBy=lastUpdatedDate&sortOrder=descending'
     );
-    var parser = new DOMParser();
-    var doc = parser.parseFromString(response.body, "application/xml");
-    return doc.querySelectorAll('entry');
+    const doc = new DOMParser().parseFromString(response.body, 'application/xml');
+    //return doc.getElementsByTagName('entry');
+
+   // Intermediate obj
+   var parser = require('fast-xml-parser');
+   var jsonObj = parser.parse(response.body);
+    return jsonObj;
   }
 
   private async getTitle(title: string) {
--- src/modules/message/message.service.ts
+++ src/modules/message/message.service.ts
@@ -13,7 +13,7 @@ export class MessageService {
   sendMessage(text: string) {
     return this.bot.telegram.sendPhoto(
       this.configService.get<string>('CHAT_ID'),
-      { url: 'https://source.unsplash.com/weekly?science' },
+      { url: 'https://source.unsplash.com/weekly?space' },
       { parse_mode: 'HTML', caption: text },
     );
   }
